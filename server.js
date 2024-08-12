ab9b406ecb75cca9dd08b70fbfdc18410a3217d9
raveenkarri <raveenkarri@gmail.com>
2024-08-12 20:58:24 +0530

    cors fix

--- server.js
+++ server.js
@@ -8,13 +8,7 @@ dbConnection();
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/users", require("./routes/UserContact"));
